db0f10043d4a22782df975259460145039002560
chenghua5 <chenghua5@jd.com>
2023-08-07 14:56:43 +0800

    阅读源码，添加注释

--- spring-cheng/src/main/java/com/cheng/Main.java
+++ spring-cheng/src/main/java/com/cheng/Main.java
@@ -1,5 +1,6 @@
 package com.cheng;
 
+import com.cheng.bean.People;
 import com.cheng.bean.User;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		User user = context.getBean("user", User.class);
+		People people = context.getBean("people", People.class);
+		System.out.println(people);
 		user.hello();
 	}
 }

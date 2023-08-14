4fbe5392ee3ca47de70828fa5e4edd130e74df56
chenghua5 <chenghua5@jd.com>
2023-08-14 11:43:16 +0800

    阅读源码，添加注释

--- spring-cheng/src/main/java/com/cheng/util/MyBeanPostProcessor.java
+++ spring-cheng/src/main/java/com/cheng/util/MyBeanPostProcessor.java
@@ -1,5 +1,6 @@
 package com.cheng.util;
 
+import com.cheng.bean.User;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
@@ -8,8 +9,9 @@ import org.springframework.beans.factory.config.InstantiationAwareBeanPostProces
 public class MyBeanPostProcessor implements BeanPostProcessor {
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("MyBeanPostProcessor.postProcessBeforeInitialization被执行！！！");
+		//System.out.println("MyBeanPostProcessor.postProcessBeforeInitialization被执行！！！");
 		if ("user".equals(beanName)){
+			System.out.println("BeanPostProcessor.postProcessBeforeInitialization方法可以提供自己的实例化逻辑！");
 			System.out.println("init before:" + bean);
 		}
 		return bean;
@@ -17,8 +19,12 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
 
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("MyBeanPostProcessor.postProcessAfterInitialization被执行！！！");
+		//System.out.println("MyBeanPostProcessor.postProcessAfterInitialization被执行！！！");
+		User user;
 		if ("user".equals(beanName)){
+			System.out.println("BeanPostProcessor.postProcessAfterInitialization方法修改了bean的Name值！");
+			user = (User) bean;
+			user.setName("李四");
 			System.out.println("init after:" + bean);
 		}
 		return bean;
